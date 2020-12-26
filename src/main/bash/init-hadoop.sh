df51c8e9de0337b17ec645347faed9def5c8c60c
esakik <social0430@gmail.com>
2020-12-27 02:36:44 +0900

    Refactoring

--- src/main/bash/init-hadoop.sh
+++ src/main/bash/init-hadoop.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-# format the NameNode
+# Reference: https://docs.cloudera.com/documentation/enterprise/5-3-x/topics/cdh_qs_yarn_pseudo.html
+
+# Format the NameNode
 sudo -u hdfs hdfs namenode -format
 
-# start HDFS
+# Start HDFS
 for x in `cd /etc/init.d ; ls hadoop-hdfs-*` ; do sudo service $x start ; done
 
-# create the directories needed for Hadoop processes
+# Create the directories needed for Hadoop processes
 sudo /usr/lib/hadoop/libexec/init-hdfs.sh
 
-# verify the HDFS File Structure
+# Verify the HDFS File Structure
 sudo -u hdfs hadoop fs -ls -R /
 
-# start YARN
+# Start YARN
 sudo service hadoop-yarn-resourcemanager start
 sudo service hadoop-yarn-nodemanager start
 sudo service hadoop-mapreduce-historyserver start
